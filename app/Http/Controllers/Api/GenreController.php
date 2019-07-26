a85a8145d7ebe2cfbc43c5a91cd12b2213b0e43f
Igor Zeljkovic <igor.zeljkovic@vivifyideas.com>
2019-07-26 11:36:09 +0200

    Implement movie filtering by genre

--- app/Http/Controllers/Api/GenreController.php
+++ app/Http/Controllers/Api/GenreController.php
@@ -4,39 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Movie;
+use App\Genre;
 
-class MovieController extends Controller
+class GenreController extends Controller
 {
-
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['only' => ['index']]);
-    }
-
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function index()
     {
-        return $request->genre
-            ? Movie::where('genre_id', $request->genre)->paginate(10)
-            : Movie::where("title", "like", "%{$request->title}%")->paginate(10);
-                    
-    }
-
-    public function visit(Movie $movie)
-    {
-        $movie->increment('visits_number');
-
-        return $movie; 
+        return Genre::all();
     }
 
     /**
